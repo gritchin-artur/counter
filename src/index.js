77f976e60d49f90e194aa95fac5c4080d6b0ed94
artur <grit4in0225@icloud.com>
2025-02-25 13:30:40 +0100

    finish counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello, world!');
+const container = document.getElementById('container');
+const displayContainer = document.getElementById('display');
+const buttonsContainer = document.getElementById('buttons');
+
+const btnArray = ['decrease', 'reset', 'increase'];
+
+btnArray.map((nameBtn) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.textContent = nameBtn;
+
+    btn.addEventListener('click', () => getControl(nameBtn));
+
+    buttonsContainer.appendChild(btn);
+});
+
+displayContainer.textContent = '0';
+
+function getControl(nameBtn) {
+    let currentValue = Number(displayContainer.textContent);
+
+    switch (nameBtn) {
+        case 'decrease':
+            if (displayContainer.textContent === '0') {
+                return;
+            } else {
+                return (displayContainer.textContent = currentValue - 1);
+            }
+        case 'reset':
+            return (displayContainer.textContent = '0');
+        case 'increase':
+            return (displayContainer.textContent = currentValue + 1);
+    }
+}
